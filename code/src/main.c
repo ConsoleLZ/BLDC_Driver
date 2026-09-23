7a52ad359f1af1edac4b3e2839117a69e7dfa64a
liuzhe <1804319025@qq.com>
2026-09-23 14:50:30 +0800

    fix

--- code/src/main.c
+++ code/src/main.c
@@ -27,21 +27,17 @@ int main(void)
     IIC_Init();
     IO_Init();
     ADC1_Init();
-    sin_generate(sin_table, SIN_TABLE_SIZE);
 
     while (1)
     {
         if (sysCnt >= 10)
         {
             sysCnt = 0;
-            if (speed_duty < SPEED_DUTY)
-                speed_duty += SPEED_RAMP_STEP; // 软起动爬坡, ~750ms到顶
 
             int16_t u = ADC_mV(ADC_CH_PA3) - 1650;
             int16_t v = ADC_mV(ADC_CH_PA7) - 1650;
             int16_t w = 0 - u - v;
             printf("u = %d, v = %d, w = %d\r\n", u, v, w);
-            test_vector();
         }
     }
 
